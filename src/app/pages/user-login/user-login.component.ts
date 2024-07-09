ee159727332e511f46f2ad22fe6a8217a0044280
devambrose <ambuhmwangi.code@gmail.com>
2024-07-09 11:48:36 +0300

    form idea

--- src/app/pages/user-login/user-login.component.ts
+++ src/app/pages/user-login/user-login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
+
+  loginForm= new FormGroup({
+    username : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required])
+  })
+  
+
+  handleFormSubmit(event:any){
+
+    event.preventDefault();
+
+
+    console.log("valid",this.loginForm.valid)
+    console.log("value",this.loginForm.value);
+
+    //{username:ambrose,password:test@!23}
+  }
+
 
 }
